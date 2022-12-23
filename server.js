9a457dce386ad78d49b2896185bb82b95ee46931
Kizilka <kirillshevchenko555@gmail.com>
2022-12-23 16:41:44 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path')
+
+
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const jsFile = fs.readFileSync(path.join(__dirname, 'static', 'js.js'));
+
 
 const server = http.createServer((req, res)=>{
-
+switch(req.url) {
+   case '/': return res.end(indexHtmlFile);
+   case '/style.css': return res.end(styleFile);
+   case '/js.js': return res.end(jsFIle);
+}
+    res.statusCode = 404;
+    return res.end('Error 404');
 })
 
-server.listen(3000)
+server.listen(3000);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket)=>{
+    console.log('a user connected. id - ' + socket.id) ;
+})
