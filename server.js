2ed8378960c1f843dfb59ab02f80f250a7a880f0
Kizilka <kirillshevchenko555@gmail.com>
2022-12-23 18:17:18 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path')
-
+const path = require('path');
 
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
 const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
-const jsFile = fs.readFileSync(path.join(__dirname, 'static', 'js.js'));
-
+const jsFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
 
 const server = http.createServer((req, res)=>{
 switch(req.url) {
-   case '/': return res.end(indexHtmlFile);
-   case '/style.css': return res.end(styleFile);
-   case '/js.js': return res.end(jsFIle);
+case '/': return res.end(indexHtmlFile);
+case '/style.css': return res.end(styleFile);
+case '/script.js': return res.end(jsFile);
 }
-    res.statusCode = 404;
-    return res.end('Error 404');
+res.statusCode = 404;
+return res.end('Error 404');
 })
 
 server.listen(3000);
 
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
 io.on('connection', (socket)=>{
-    console.log('a user connected. id - ' + socket.id) ;
+console.log('a user connected. id - ' + socket.id);
+
+socket.on('new_message', (message)=>{
+io.emit('message', message);
+})
 })
